9df7f0742397e5e4ec14d25de2a7ac13b6be507a
Rishal Hurbans <rishal.hurbans@gmail.com>
2016-03-16 22:04:13 +0200

    Added maze generation rule.

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ canvas.strokeStyle = '#E1E1E1';
 canvas.fillStyle = '#00A2EF';
 var cells = [];
 var cellScale = 10;
-var updateSpeed = 50;
+var updateSpeed = 100;
 var totalAlive = 1;
 var totalLastAlive = 1;
 var isMouseDown = false;
+var maxGeneration = 100;
+var currentGeneration = 0;
 
 init();
 
@@ -27,7 +29,7 @@ function init() {
     // Prefilled cells
     [
         // Gosper glider gun
-        [1, 5],[1, 6],[2, 5],[2, 6],[11, 5],[11, 6],[11, 7],[12, 4],[12, 8],[13, 3],[13, 9],[14, 3],[14, 9],[15, 6],[16, 4],[16, 8],[17, 5],[17, 6],[17, 7],[18, 6],[21, 3],[21, 4],[21, 5],[22, 3],[22, 4],[22, 5],[23, 2],[23, 6],[25, 1],[25, 2],[25, 6],[25, 7],[35, 3],[35, 4],[36, 3],[36, 4],
+        [1, 5],[1, 6],[2, 5],[2, 6],[11, 5],[11, 6],[11, 7],[12, 4],[12, 8],[13, 3],[13, 9],[14, 3],[14, 9],[15, 6],[16, 4],[16, 8],[17, 5],[17, 6],[17, 7],[18, 6],[21, 3],[21, 4],[21, 5],[22, 3],[22, 4],[22, 5],[23, 2],[23, 6],[25, 1],[25, 2],[25, 6],[25, 7],[31, 3],[31, 4],[31, 3],[31, 4],
         
         // Random cells
         // If you wait enough time these will eventually take part
@@ -70,13 +72,23 @@ function update() {
     cells.forEach(function(row, x) {
         result[x] = [];
         row.forEach(function(cell, y) {
-            var alive = 0,
-                count = _countNeighbours(x, y);
+            var alive = 0;
+            var count = _countNeighbours(x, y);
             
-            if (cell > 0) {
+            /*if (cell > 0) {
                 alive = count === 2 || count === 3 ? 1 : 0;
             } else {
                 alive = count === 3 ? 1 : 0;
+            }*/
+
+            if(cell > 0){
+                if(count > 0 && count < 5) {
+                    alive = 1;
+                }
+            }else {
+                if(count === 3) {
+                    alive = 1;
+                }
             }
             
             result[x][y] = alive;
@@ -110,16 +122,24 @@ function doMouseMove(event) {
 
 function draw() {
     canvas.clearRect(0, 0, 4000, 4000);
+    var logOutput = "";
     cells.forEach(function(row, x) {
         row.forEach(function(cell, y) {
             canvas.beginPath();
             canvas.rect(x * cellScale, y * cellScale, cellScale, cellScale);
             if (cell) {
                 canvas.fill();
+                logOutput += '#';
             } else {
-                canvas.stroke();
+                console.log('.');
+                logOutput += '.';
             }
         });
+        logOutput += '\n';
     });
-    setTimeout(function() {update();}, updateSpeed);
+    logOutput += '\n\n';
+    console.log(logOutput);
+    currentGeneration++;
+    if(currentGeneration < maxGeneration)
+        setTimeout(function() {update();}, updateSpeed);
 }
